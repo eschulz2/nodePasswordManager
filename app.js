8895b1e7fcf4bb76c4421be6397537832e7f95ec
studio three <eric@thirddegreefitness.com>
2016-01-15 14:08:07 -0600

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+function createAccount(account) {
+	var accounts = storage.getItemSync('accounts');
+
+	if (typeof accounts === 'undefined') {
+		accounts = [];
+	}
+
+	accounts.push(account);
+	storage.setItemSync('accounts', accounts);
+
+	return account;
+}
+
+function getAccount(accountName) {
+	var accounts = storage.getItemSync('accounts');
+  var matchedAccount;
+
+  accounts.forEach(function (account) {
+  	if (account.name === accountName) {
+  		matchedAccount = account;
+  	}
+  });
+  return matchedAccount;
+}
+
+// createAccount({
+//   name: "Facebook",
+//   username: "eric@gmail.com",
+//   password: "12345678"
+// });
+
+var facebookAccount = getAccount('Facebook');
+console.log(facebookAccount);
